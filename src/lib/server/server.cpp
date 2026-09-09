5937176c4ce33ac773c964cc1e3aed4c23655a65
Z1nK <llobesa@gmail.com>
2026-09-09 22:16:55 +0200

    refactor: make TcpServer session handling generic

--- src/lib/server/server.cpp
+++ src/lib/server/server.cpp
@@ -1,7 +1,6 @@
 #include "server.hpp"
 
-#include <iostream>
-#include <string>
+#include <utility>
 
 using boost::asio::awaitable;
 using boost::asio::co_spawn;
@@ -9,7 +8,7 @@ using boost::asio::detached;
 using boost::asio::use_awaitable;
 using boost::asio::ip::tcp;
 
-TcpServer::TcpServer(boost::asio::io_context& io, Settings settings) : io_(io), settings_(settings) {}
+TcpServer::TcpServer(boost::asio::io_context& io, Settings settings) : io_(io), settings_(std::move(settings)) {}
 
 void TcpServer::run() {
   co_spawn(io_, acceptLoop(), detached);
@@ -21,19 +20,6 @@ awaitable<void> TcpServer::acceptLoop() {
 
   for (;;) {
     tcp::socket socket = co_await acceptor.async_accept(use_awaitable);
-    co_spawn(executor, handleSession(std::move(socket)), detached);
+    co_spawn(executor, settings_.on_session(std::move(socket)), detached);
   }
-}
-
-awaitable<void> TcpServer::handleSession(tcp::socket socket) {
-  boost::asio::streambuf buffer;
-  co_await boost::asio::async_read_until(socket, buffer, "\n", use_awaitable);
-
-  std::istream is(&buffer);
-  std::string line;
-  std::getline(is, line);
-  std::cout << "[server] received: \"" << line << "\"\n";
-
-  std::string reply = line + "\n";
-  co_await boost::asio::async_write(socket, boost::asio::buffer(reply), use_awaitable);
 }
--- src/lib/server/server.hpp
+++ src/lib/server/server.hpp
@@ -2,11 +2,15 @@
 #include <boost/asio.hpp>
 
 #include <cstdint>
+#include <functional>
 
 class TcpServer final {
 public:
+  using SessionHandler = std::function<boost::asio::awaitable<void>(boost::asio::ip::tcp::socket)>;
+
   struct Settings {
     std::uint16_t port;
+    SessionHandler on_session;
   };
 
   explicit TcpServer(boost::asio::io_context& io, Settings settings);
@@ -15,7 +19,6 @@ public:
 
 private:
   boost::asio::awaitable<void> acceptLoop();
-  static boost::asio::awaitable<void> handleSession(boost::asio::ip::tcp::socket socket);
 
   boost::asio::io_context& io_;
   Settings settings_;
